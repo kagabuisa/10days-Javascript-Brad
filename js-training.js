fe96cbcd21a789b468bea5f967a5ce0046f3c94c
kagabuisa <kagabuisa@gmail.com>
2018-11-24 23:27:05 +0300

    JS first commit

---
+++ js-training.js
@@ -0,0 +1,21 @@
+// function greet(called) {
+//     alert("Hello James " + ", my name is " + called + "!!!!");
+//     called++;
+// }
+
+// greet("Bond");
+// greet("James");
+
+
+// function trippleMe(x) {
+//     return (x * 3);
+// }
+
+// let yy = trippleMe(22);
+// alert(yy);
+
+document.addEventListener("click", myB)
+
+function myB() {
+    alert("You have clicked !!!!, Click again")
+}
