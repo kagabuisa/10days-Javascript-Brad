fca6af5ae536ae68c795b3dd97b80cfda5a880f6
kagabuisa <kagabuisa@gmail.com>
2018-11-25 17:06:05 +0300

    commit js upto day 4

--- js-training.js
+++ js-training.js
@@ -14,8 +14,20 @@
 // let yy = trippleMe(22);
 // alert(yy);
 
-document.addEventListener("click", myB)
+// document.addEventListener("click", myB)
 
-function myB() {
-    alert("You have clicked !!!!, Click again")
-}
+// function myB() {
+//     alert("You have clicked !!!!, Click again")
+// }
+
+
+// Array
+
+// for (i = 0; i < myWords.length; i++) {
+//     console.log(myWords[i]);
+// }
+
+let myWords = ["Kampalan", "Mbarara", "Arua", "Masaka"]
+myWords += myWords.push("Kasese")
+
+console.log(myWords)
