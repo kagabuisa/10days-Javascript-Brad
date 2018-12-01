19f06bb75ddb53000a538b312c5bddc42cb323eb
kagabuisa <kagabuisa@gmail.com>
2018-12-01 17:08:29 +0300

    commit

--- js-training.js
+++ js-training.js
@@ -1,62 +1,8 @@
-let pets = [{
-        name: "meaowalot",
-        species: "Cat",
-        age: 2
-    },
-    {
-        name: "barksalot",
-        species: "Dog",
-        age: 2
-    },
-    {
-        name: "mowalot",
-        species: "Cow",
-        age: 6
-    }
-]
+// let myNumbers = [2, 50, 2000]
+// let doubleNumbers = myNumbers.map((x) =>
+//     return x * 0.5)
 
+// console.log(doubleNumbers)
 
-
-console.log(pets.push({
-    name: "growsalot",
-    species: "Dog",
-    age: 3
-}))
-console.log(pets)
-
-let test = pets.map(nameOnly)
-
-function nameOnly(x) {
-
-    return x.name
-}
-
-// console.log(test)
-
-let test2 = pets.filter(onDogs)
-
-function onDogs(x) {
-    return x.species == "Dog"
-
-}
-
-function onlyBabyDogs(x) {
-    return x.age < 4
-
-}
-
-let babydogs = test2.filter(onlyBabyDogs).map(nameOnly)
-
-console.log(babydogs)
-
-function addApples(a, b) {
-    return a + b
-}
-
-addApples(3, 5)
-
-function avg(addApples, c) {
-    return addApples(a, b) / 2
-}
-
-avg(addApples(3, 5), 2)
+let myName = "Kagabu Isa";
+console.log(`Hello, people ${function(){let b=3;if(b>2){return b*50}}()} is my name.`)
